1a2e3b4d43a7659fa5b65c59274e1322520a0e11
jauharfz <jauharf14@gmail.com>
2025-03-11 00:04:58 +0700

    adding FruitCode class

---
+++ 04_Automata_dan_Table-Driven_Construction/FruitCodes.cs
@@ -0,0 +1,35 @@
+﻿namespace _04_Automata_dan_Table_Driven_Construction;
+
+public class FruitCodes
+{
+    //bikin dictionary, dengan key dan value string
+    private Dictionary<string, string> _fruitTable = new Dictionary<string, string>()
+    {
+        {"Apel", "A00"},
+        {"Aprikot", "B00"},
+        {"Alpukat", "C00"},
+        {"Pisang", "D00"},
+        {"Paprika", "E00"},
+        {"Blackberry", "F00"},
+        {"Ceri", "H00"},
+        {"Kelapa", "I00"},
+        {"Jagung", "J00"},
+        {"Kurma", "K00"},
+        {"Durian", "L00"},
+        {"Anggur", "M00"},
+        {"Melon", "N00"},
+        {"Semangka", "O00"}
+    };
+    //getternya jgn lupa
+    public string GetFruitCode(string fruitName)
+    {
+        if (_fruitTable.ContainsKey(fruitName))
+        {
+            return _fruitTable[fruitName];
+        }
+        else
+        {
+            return "Mff kode tidak ditemukan";
+        }
+    }
+}
--- 04_Automata_dan_Table-Driven_Construction/Program.cs
+++ 04_Automata_dan_Table-Driven_Construction/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿using _04_Automata_dan_Table_Driven_Construction;
+class Program
+{
+    static void Main()
+    {
+        FruitCodes fruitCodes = new FruitCodes();
+        Console.WriteLine("Enter fruit name:");
+        var userInput = Console.ReadLine();
+        Console.WriteLine(fruitCodes.GetFruitCode(userInput));
+        Console.ReadKey();
+    }
+}
